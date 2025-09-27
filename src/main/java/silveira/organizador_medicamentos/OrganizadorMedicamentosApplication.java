d79d17e955757089c4bdd412d12ae2a69c0f3f59
Silveira <gabrielsantossilveira@hotmail.com>
2025-09-26 23:34:00 -0300

    first commit

---
+++ src/main/java/silveira/organizador_medicamentos/OrganizadorMedicamentosApplication.java
@@ -0,0 +1,13 @@
+package silveira.organizador_medicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorMedicamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorMedicamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silveira/organizador_medicamentos/OrganizadorMedicamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package silveira.organizador_medicamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorMedicamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
